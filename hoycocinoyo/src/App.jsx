a2ee0c2df1c73a448b288578c9ab783ff314427f
elopezfilippini <elopezfilippini@gmail.com>
2025-01-07 21:14:13 -0300

    login inicio

---
+++ hoycocinoyo/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Login from './Login'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      
+      </div>
+    
+      <div className="card">
+      
+      <h1 class="titulo">Hoy cocino yo</h1>
+
+      </div>
+      <p className="subtitulo">
+        Logueése para poder ingresar
+      </p>
+
+       {/* RUTAS USERS */}
+      <Routes>
+      <Route path="/" element={<Login />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ hoycocinoyo/src/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux";
+
+function Login()
+{
+    const navigate = useNavigate();
+    const [access, setAccess] = useState(false);
+    const EMAIL = 'ejemplo@gmail.com';
+    const PASSWORD = '1234';
+    const [userData,SetUserData] = useState({email:"",password:""})
+    const [error,setErrors] = useState({})
+    function login(userData) {
+        if (userData.password === PASSWORD && userData.email === EMAIL) {
+           setAccess(true);
+           navigate('/inicio');
+        }  
+        else window.alert("La contraseña introducida no es valida")}
+
+        function HandleOnChange (e){
+            console.log(e.target.name)
+            const nombre = e.target.name;
+            SetUserData({...userData,[nombre]: e.target.value});
+            console.log (userData)
+            setErrors(validate({...userData,[nombre]: e.target.value}))
+        }
+        function Handleclick(){
+            navigate('/inicio')
+        }
+        
+        function handleSubmit(e){
+            e.preventDefault()
+            console.log(userData.password)
+            console.log(userData.email)
+            console.log(login(userData))
+        }
+
+return (
+<div>
+    <label style={{color:"black"}}>Email : </label> <input name="email" onChange={HandleOnChange}></input>
+    <label style={{color:"black"}} >    Password: </label> <input name="password"  type  ="password"onChange={HandleOnChange}></input>
+    <hr style={{margin:"15px"}}></hr>   
+    <a1 style={{color:"red"}}>{error.email ? error.email: null} </a1>
+    <a1 style={{color:"red"}}>{error.password ? error.password: null} </a1>
+    <br></br>
+    <button type="submit" style={{margin:"5px"}} onClick={handleSubmit}> Enviar </button>
+    </div>
+);
+
+
+
+}
+export default Login;
